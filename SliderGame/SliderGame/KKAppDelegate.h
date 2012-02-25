7e771934bb2694ee0e78fc355fca3ed5782cafcf
Kaushal Kantawala <kaushal@funzio.com>
2012-02-25 15:50:31 -0800

    Create the iOS project

---
+++ SliderGame/SliderGame/KKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KKAppDelegate.h
+//  SliderGame
+//
+//  Created by Kaushal Kantawala on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SliderGame/SliderGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SliderGame
+//
+//  Created by Kaushal Kantawala on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KKAppDelegate class]));
+    }
+}
